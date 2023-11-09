8ead2874ce07d1a66dfd8b6a8cd5b1062034a3e5
dohko09 <wherrera3@utmachala.edu.ec>
2023-11-09 17:26:00 -0500

    correción de errores

---
+++ src/pages/Login.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+
+const Login: React.FC = () => {
+    return (
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Inicio de Sesión</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    </IonPage>
+    
+    );
+};
+export default Login;
---
+++ src/pages/Register.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { IonCol, IonContent, IonGrid, IonHeader, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+
+const Register: React.FC = () => {
+    return (
+    <IonPage>
+        <IonHeader>
+            <IonToolbar>
+                <IonTitle>Registrarse</IonTitle>
+            </IonToolbar>
+        </IonHeader>
+        <IonContent>
+            <IonGrid>
+                <IonRow>
+                    <IonCol>
+                        
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
+        </IonContent>
+    </IonPage>
+    
+    );
+};
+export default Register;
